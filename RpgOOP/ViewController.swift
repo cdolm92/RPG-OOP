9157d3dde7b03f0a07b8823a09424164af1d6ab1
cdolm92 <cdolm92@gmail.com>
2016-02-05 02:05:51 -0500

    updated UI

--- RpgOOP/ViewController.swift
+++ RpgOOP/ViewController.swift
@@ -10,12 +10,81 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var printLbl: UILabel!
+    
+    @IBOutlet weak var playerHpLbl: UILabel!
+    
+    @IBOutlet weak var enemyHpLbl: UILabel!
+    
+    @IBOutlet weak var enemyImg: UIImageView!
+    
+    @IBOutlet weak var chestBtn: UIButton!
+    
+    var player: Player!
+    var enemy: Enemy!
+    var chestMessage: String?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        player = Player(name: "DirtyLaundry21", hp: 110, attackPwr: 20)
+        playerHpLbl.text = "\(player.hp) HP"
+    }
+    
+    func generateRandomEnemy() {
+        let rand = Int(arc4random_uniform(2))
+        
+        if rand == 0 {
+            enemy = Kimara(startingHP: 50, attackPwr: 12)
+        } else {
+            enemy = DevilWizard(startingHP: 60, attackPwr:  15)
+        }
     }
 
+
+    @IBAction func onChestTapped(sender: AnyObject) {
+        chestBtn.hidden = true
+        printLbl.text = chestMessage
+        NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: "generateRandomEnemy", userInfo: nil, repeats: false)
+        
+    }
   
+    @IBAction func attackTapped(sender: AnyObject) {
+        
+        if enemy.attemptAttack(player.attackPwr){
+            printLbl.text = "Attcked \(enemy.type) for \(player.attackPwr) HP"
+        } else {
+            printLbl.text = "Attack was unsuccessful"
+        }
+        
+        if let loot = enemy.dropLoot() {
+            player.inventory.append(loot)
+            chestMessage = "\(player.name) found \(loot)"
+            chestBtn.hidden = false
+        }
+        
+        if !enemy.isAlive {
+            enemyHpLbl.text = ""
+            printLbl.text = "Killed \(enemy.type)"
+            enemyImg.hidden = true
+        }
+        
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
